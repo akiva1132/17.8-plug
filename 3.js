c8467d28c706b524748ae55ff1bdd41d9b8a63dc
Akiva Kraus <125896864+akiva1132@users.noreply.github.com>
2023-08-17 17:38:08 +0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,9 @@
+fetch('https://www.ynet.co.il/news/article/bjjprkuh2#autoplay')
+  .then(response => response.text())
+  .then(htmlContent => {
+    // כאן אתה יכול לעשות מה שאתה רוצה עם ה-tHTML שנבאט מהשרת
+    console.log(htmlContent);
+  })
+  .catch(error => {
+    console.error('An error occurred', error);
+  });
