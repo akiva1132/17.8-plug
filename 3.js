31eeba0dd6dbd031d84de137a465e13946e6f978
Akiva Kraus <125896864+akiva1132@users.noreply.github.com>
2023-08-17 18:11:47 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,9 +1,10 @@
-fetch('https://www.ynet.co.il/news/article/bjjprkuh2#autoplay')
-  .then(response => response.text())
-  .then(htmlContent => {
-    // כאן אתה יכול לעשות מה שאתה רוצה עם ה-tHTML שנבאט מהשרת
-    console.log(htmlContent);
-  })
-  .catch(error => {
-    console.error('An error occurred', error);
-  });
+// fetch('https://www.ynet.co.il/news/article/bjjprkuh2#autoplay')
+//   .then(response => response.text())
+//   .then(htmlContent => {
+//     // כאן אתה יכול לעשות מה שאתה רוצה עם ה-tHTML שנבאט מהשרת
+//     console.log(htmlContent);
+//   })
+//   .catch(error => {
+//     console.error('An error occurred', error);
+//   });
+console.log("hello");
