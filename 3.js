a9fa6b455b6a65d8e63895a3b115a0488ecda823
Akiva Kraus <125896864+akiva1132@users.noreply.github.com>
2023-08-17 18:38:26 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,12 +1,5 @@
-// fetch('https://www.ynet.co.il/news/article/bjjprkuh2#autoplay')
-//   .then(response => response.text())
-//   .then(htmlContent => {
-//     // כאן אתה יכול לעשות מה שאתה רוצה עם ה-tHTML שנבאט מהשרת
-//     console.log(htmlContent);
-//   })
-//   .catch(error => {
-//     console.error('An error occurred', error);
-//   });
+const express = require("express");
+const app = express();
 
 app.get("/users", async (req, res) => {
 res.json({"hello": "world"});
