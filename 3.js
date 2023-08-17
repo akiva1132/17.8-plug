6e917634df80350712d848f863d5d034ddfdd326
Akiva Kraus <125896864+akiva1132@users.noreply.github.com>
2023-08-17 19:34:08 +0300

    Add files via upload

--- 3.js
+++ 3.js
@@ -1,11 +1,11 @@
 const express = require("express");
-// const fetch = require("node-fetch");
+const axios = require("axios");
 const app = express();
 
 app.get("/", async (req, res) => {
   try {
-    const response = await fetch('https://www.ynet.co.il/news/article/bjjprkuh2#autoplay');
-    const htmlContent = await response.text();
+    const response = await axios.get('https://www.ynet.co.il/news/article/bjjprkuh2#autoplay');
+    const htmlContent = response.data;
     res.send(htmlContent);
   } catch (error) {
     console.error('An error occurred', error);
